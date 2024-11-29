2248ca81e0fbf7e4d8f582569d77395a532451bc
Shirin <esshirin486@gmail.com>
2024-11-29 12:36:27 +0100

    Main-Content

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-function App() {
+import DiaryEntriesList from "./Components/DiaryEntriesList";
+
+const App = () => {
   return (
-    <div class="bg-base-200 flex items-center justify-center min-h-screen">
-      <div class="text-center">
-        <h1 class="text-5xl font-bold text-primary">Welcome</h1>
-        <p class="mt-4 text-lg text-secondary">
-          This is your personal digital Dary
-        </p>
+    <div>
+      {/* <Navbar /> */}
+      <div className="bg-base-200 flex items-center justify-center min-h-screen">
+        <div className="text-center">
+          <h1 className="text-5xl font-bold text-primary">Welcome</h1>
+          <p className="mt-4 text-lg text-secondary">
+            This is your personal digital Diary
+          </p>
+          <DiaryEntriesList />
+        </div>
       </div>
+      {/* <Footer /> */}
     </div>
   );
-}
+};
 
 export default App;
