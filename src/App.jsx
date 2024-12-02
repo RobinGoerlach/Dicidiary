073f2c1689c9a5f7c534a69591fa0bb7ff7f0531
MathiasZ91git config --global user.email mzeibig@hotmail.deit commit -minitialgit commit -minitialgit commit -minitialgit config --global user.name MathiasZ91 <mzeibig@hotmail.de>
2024-12-02 13:59:30 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import React from "react";
 import NavBar from "./components/NavBar";
 import DiaryEntriesList from "./Components/DiaryEntriesList";
 import Footer from "./components/Footer";
-import Modal from "./components/Modal";
+
 
 const App = () => {
   return (
 
       <div>      
           <NavBar/>
-          <Modal />
+          
           <DiaryEntriesList />
           <Footer />
     </div>
